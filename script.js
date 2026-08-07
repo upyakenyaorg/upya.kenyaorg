e2c7f03006e36e30638bcbbf7368c628168d1e0d
upyakenyaorg <upya.kenyaorg@gmail.com>
2026-08-07 11:02:49 +0300

    Update script.js

--- script.js
+++ script.js
@@ -294,3 +294,16 @@ year.innerHTML=new Date().getFullYear();
 // =========================
 
 console.log("Welcome to UPYA Website");
+const slides=document.querySelectorAll(".hero-slide");
+
+let currentSlide=0;
+
+setInterval(()=>{
+
+slides[currentSlide].classList.remove("active");
+
+currentSlide=(currentSlide+1)%slides.length;
+
+slides[currentSlide].classList.add("active");
+
+},5000);
