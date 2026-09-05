34ab773b143245b7757054da7f66fb77513afba5
upyakenyaorg <upya.kenyaorg@gmail.com>
2026-09-05 18:10:57 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
+/* ==============================
+   UPYA NAVIGATION
+================================ */
 
+function toggleMenu(){
+
+    const nav =
+        document.querySelector(".nav-links");
+
+    nav.classList.toggle("active");
+
+}
+
+
+/* Close mobile menu after clicking */
+
+document
+    .querySelectorAll(".nav-links a")
+    .forEach(function(link){
+
+        link.addEventListener("click",function(){
+
+            document
+                .querySelector(".nav-links")
+                .classList.remove("active");
+
+        });
+
+    });
